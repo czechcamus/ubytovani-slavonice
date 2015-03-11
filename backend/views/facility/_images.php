dadadb642e6099c6ceb58a72af72e985be32b423
czechcamus <ja@camus.cz>
2015-03-11 23:05:38 +0100

    doma 11.3.15, 23:05

--- backend/views/facility/_images.php
+++ backend/views/facility/_images.php
@@ -1,7 +1,29 @@
 <?php
 
+use kartik\widgets\FileInput;
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 /* @var $model backend\models\FacilityForm */
 ?>
 
-Zatím nic
+<?= FileInput::widget([
+	'name'          => 'images[]',
+	'options'       => [
+		'multiple'    => true
+	],
+	'pluginOptions' => [
+		'allowedFileType' => ['image'],
+		'msgInvalidFileType' => Yii::t('back', 'Invalid File Type'),
+		'allowedFileExtensions' => ['jpg', 'gif', 'png'],
+		'msgInvalidFileExtension' => Yii::t('back', 'Invalid Image Type'),
+		'uploadUrl'       => Url::to(['facility/file-upload']),
+		'uploadExtraData' => [
+			'facility_id' => $model->facility_id
+		],
+		'maxFileCount'    => 10,
+		'dropZoneTitle' => Yii::t('back', 'Drag & drop files here …')
+	]
+]);
+
+//TODO tady budou již nahrané obrázky s možností úprav
