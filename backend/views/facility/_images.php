215d9bbc153333f2f4ca36c4a63446ef17f4c320
czechcamus <ja@camus.cz>
2015-03-12 23:27:03 +0100

    doma 12.3.15, 23:26

--- backend/views/facility/_images.php
+++ backend/views/facility/_images.php
@@ -13,17 +13,34 @@ use yii\helpers\Url;
 		'multiple'    => true
 	],
 	'pluginOptions' => [
+		'msgSelected' => Yii::t('back', '{n} files selected'),
 		'allowedFileType' => ['image'],
 		'msgInvalidFileType' => Yii::t('back', 'Invalid File Type'),
 		'allowedFileExtensions' => ['jpg', 'gif', 'png'],
 		'msgInvalidFileExtension' => Yii::t('back', 'Invalid Image Type'),
-		'uploadUrl'       => Url::to(['facility/file-upload']),
+		'maxFileSize' => 2000000,
+		'msgSizeTooLarge' => Yii::t('back', 'File "{name}" ({size} KB) exceeds maximum allowed upload size of {maxSize} KB. Please retry your upload!'),
+		'msgFilesTooMany' => Yii::t('back', 'Number of files selected for upload ({n}) exceeds maximum allowed limit of {m}. Please retry your upload!'),
+		'uploadUrl'       => Url::to(['image/upload']),
 		'uploadExtraData' => [
 			'facility_id' => $model->facility_id
 		],
 		'maxFileCount'    => 10,
-		'dropZoneTitle' => Yii::t('back', 'Drag & drop files here …')
+		'browseLabel' => Yii::t('back', 'Browse....'),
+		'removeLabel' => Yii::t('back', 'Remove'),
+		'removeTitle' => Yii::t('back', 'Clear selected files'),
+		'uploadLabel' => Yii::t('back', 'Upload'),
+		'uploadTitle' => Yii::t('back', 'Upload selected files'),
+		'dropZoneTitle' => Yii::t('back', 'Drag & drop files here …'),
+		'fileActionSettings' => [
+			'removeTitle' => Yii::t('back', 'Remove file'),
+			'uploadTitle' => Yii::t('back', 'Upload file'),
+			'indicatorNewTitle' => Yii::t('back', 'Not uploaded yet'),
+            'indicatorSuccessTitle' => Yii::t('back', 'Uploaded'),
+            'indicatorErrorTitle' => Yii::t('back', 'Upload Error'),
+            'indicatorLoadingTitle' => Yii::t('back', 'Uploading ...')
+		]
 	]
 ]);
 
-//TODO tady budou již nahrané obrázky s možností úprav
+//TODO tady budou již nahrané obrázky s možností úprav (listView)
