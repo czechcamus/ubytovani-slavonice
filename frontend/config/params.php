8e986f74315b2b7a6cc975bfd20e1390713840f4
czechcamus <czechcamus@gmail.com>
2015-07-02 15:00:21 +0200

    kancl  2.7.15, 15:00

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'adminEmail' => 'ja@camus.cz',
-	'breadCrumbsDelimiter' => ' <i class="material-icons orange-text tiny">keyboard_arrow_right</i>'
+	'breadCrumbsDelimiter' => ' <i class="material-icons orange-text tiny">keyboard_arrow_right</i>',
+    'imageUrl' => 'http://ubytovani.d1net.cz/backend/web/uploads/',
 ];
